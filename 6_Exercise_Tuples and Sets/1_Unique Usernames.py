b435a605fd4beb699689a713eb060889be2b11b0
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-08 20:44:02 +0200

    Python Tuples and Sets
    Python Tuples and Sets

---
+++ 6_Exercise_Tuples and Sets/1_Unique Usernames.py
@@ -0,0 +1,14 @@
+# nums = int(input())
+# unique_names = set()
+#
+# for num in range(nums):
+#     name = input()
+#     unique_names.add(name)
+# print("\n".join(unique_names))
+
+nums = int(input())
+unique_names = set()
+
+for num in range(nums):
+    unique_names.add(input())
+print("\n".join(unique_names))
