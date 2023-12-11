515f979c6eeb0a62ef8bbc38b845aa16a842919f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-11 19:18:17 +0200

    Python Multidimensional Lists
    Python Multidimensional Lists

---
+++ 8_Multidimensional Lists/3_Flattening Matrix.py
@@ -0,0 +1,26 @@
+# rows = int(input())
+# matrix = []
+# new_matrix = []
+# for row in range(rows):
+#     matrix.append([int(x) for x in input().split(", ")])
+#
+# for j in range(len(matrix)):
+#     new_matrix += matrix[j]
+# print(new_matrix)
+
+# rows = int(input())
+# my_list = []
+#
+# for row in range(rows):
+#     nums = [int(el) for el in input().split(", ")]
+#     my_list += nums
+# print(my_list)
+
+#
+# rows = int(input())
+# my_list = []
+#
+# for row in range(rows):
+#     nums = [int(el) for el in input().split(", ")]
+#     my_list.extend(nums)
+# print(my_list)
