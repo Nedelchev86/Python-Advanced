6a726a69c910d65f03186464f545fcbed548862a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-12 06:20:56 +0200

    Python Exercises Multidimensional Lists
    Python Exercises Multidimensional Lists

---
+++ 9_Exercises_Multidimensional Lists/3_2x2 Squares in Matrix.py
@@ -0,0 +1,33 @@
+# rows, cols = [int(x) for x in input().split()]
+# matrix = []
+#
+# for _ in range(rows):
+#     matrix.append(input().split())
+#
+# searched = []
+#
+# for row in range(rows-1):
+#     for col in range(cols -1):
+#         if matrix[row][col] == matrix[row][col+1] == matrix[row +1][col] == matrix[row+1][col +1]:
+#             find = matrix[row][col], matrix[row][col+1], matrix[row +1][col], matrix[row+1][col +1]
+#             searched.append(find)
+#
+# print(len(searched))
+
+
+
+rows, cols = [int(x) for x in input().split()]
+matrix = []
+
+for _ in range(rows):
+    matrix.append(input().split())
+
+searched = 0
+
+for row in range(rows-1):
+    for col in range(cols -1):
+        if matrix[row][col] == matrix[row][col+1] == matrix[row +1][col] == matrix[row+1][col +1]:
+            find = matrix[row][col], matrix[row][col+1], matrix[row +1][col], matrix[row+1][col +1]
+            searched += 1
+
+print(searched)
