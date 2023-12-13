af89a6dff46a0b74ea0bbfeec22361f473758ab9
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-13 12:40:42 +0200

    Python Functions Advanced
    Python Functions Advanced

---
+++ 11_Functions Advanced/1_Multiplication Function2.py
@@ -0,0 +1,17 @@
+# from functools import reduce
+#
+# def multiply(*args):
+#     return reduce(lambda a, b: a * b, args)
+
+
+
+
+from functools import reduce
+import operator
+def multiply(*args):
+    return reduce(operator.mul, args)
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
